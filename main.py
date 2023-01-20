b5cb4dfc6d483a7eb3a406955c79ff0c2ff122a9
Odidi27 <73470993+Benner27@users.noreply.github.com>
2023-01-20 11:53:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from models import TrainingData, IdeaFunction
 from function import FunctionManager, FunctionManagerIterator
 from regression import minimise_loss, find_classification
 from plotting import plot_ideal_functions, plot_points_with_their_ideal_function
+
 train_data_path = 'data/train_datasetsPyy.csv'
 ideal_funcs_path = 'data/ideal_datasetsPy.csv'
 
-# Important constant
+# Constant criterion assignment factor
 Exclusive_Factor = math.sqrt(2)
 
 if __name__ == "__main__":
@@ -42,30 +43,31 @@ if __name__ == "__main__":
         ideal_functions.append(ideal_function)
 
     # Classification is utilized to map points
-    plot_ideal_functions(ideal_functions, "plots/train_and_ideal")
+    create_ideal_functions_plots(ideal_functions, "plots/train_and_ideal")
 
     # FunctionManager reuses assembled CSV file.
     # Assessment of every test data point that are iterated through function object
     # At location [0], different functions are reduced to a distinct "Function"
     test_path = "data/datasetsPy.csv"
-    test_function_manager = FunctionManager(path_of_csv=test_path)
+    test_function_manager = FunctionManager(csv_path=test_path)
     test_function = test_function_manager.functions[0]
 
     # Dictionaries contain findings for every classification point
     # Overview: list of dictionaries stowed in points_with_ideal_function
     points_with_ideal_function = []
     for point in test_function:
-        ideal_function, delta_y = find_classification(point=point, ideal_functions=ideal_functions)
+        ideal_function, delta_y = classify_point(point=point, ideal_fn_list=ideal_functions)
         result = {"point": point, "classification": ideal_function, "delta_y": delta_y}
         points_with_ideal_function.append(result)
 
     # Utilizing corresponding classification function to map points
-    plot_points_with_their_ideal_function(points_with_ideal_function, "plots/point_and_ideal")
+    create_plot_points_with_their_ideal_function(points_with_ideal_function, "plots/point_and_ideal")
 
     # Finalize by utilizing dict object to note it to a SQLite database
     # Utilization of SQLAlchemy technique with a MetaData object process
     Utils.write_deviation_results_to_db(points_with_ideal_function)
     print("main.db: Well-equipped SQLite file")
     print("train_and_ideal.html: Observe training data as scatter and the most convenient ideal function as curve")
+    
     print("points_and_ideal.html: Contemplate matching ideal function for the points and frame distance inbetween them")
     print("Victorious Assignment accomplishment")
