87f2fe76a673bf1bab6bd3e40a73924d0abf56c2
Odidi27 <73470993+Benner27@users.noreply.github.com>
2023-01-20 21:37:00 +0100

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -1,6 +1,7 @@
 def calculate_squared_error(first_function, second_function):
     """ Computes squared error to a different function
-    :param: other_function
+    :param:first_function: First function dataframe
+    :param: second_function: Second function dataframe
     :return: complete_deviation from squared error. 
     """
     diff = second_function - first_function
