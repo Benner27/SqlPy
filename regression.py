a7e3d91e4e83c7110673d2f72d907aedebf82ef5
Odidi27 <73470993+Benner27@users.noreply.github.com>
2023-01-20 13:58:07 +0100

    Update regression.py

--- regression.py
+++ regression.py
@@ -4,6 +4,7 @@ from function import IdealFunction
 def minimise_loss(training_function, loss_function, list_of_candidate_functions):
     """ IdealFunction established on a training function and a list of ideal functions
     :param training_fn: training function
+    
     :param error_fn: utilizes error minimization
     :param candidate_fn_list: Ideal functions list
     :return: a IdealFunction object with least error. """
@@ -23,6 +24,7 @@ def minimise_loss(training_function, loss_function, list_of_candidate_functions)
 def classify_point(point, ideal_fn_list):
     """ Points within the Ideal objects and their tolerance of a closest classification to lead to computation
     :param ideal_fn_list: list of IdealFunction objects
+   
     :param point: Contain an "x" dict object and a "y" dict object
     :return:Comprises a tuple encompassing min available closest classification and the distance. """
     closest_classification = None
